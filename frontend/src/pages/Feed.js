561556ede8ffa9e3bdbec3c05d6868a8aa6b8554
Fabio Jordão <fabiosjordao@gmail.com>
2019-06-16 12:48:52 -0300

    Frontend

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+import './Feed.css';
+
+class Feed extends Component {
+    render() {
+        return (
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Nome</span>
+                            <span className="place">place</span>
+                        </div>
+                        <img src={more} alt="Mais"></img>
+                    </header>
+                    <img src="http://localhost:3333/files/Fernando-de-Noronha.jpg" alt="" />
+                
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+                        <strong>900 curtidas</strong>
+                        <p>
+                            Descrição do post!
+                            <span>#Estudo #React</span>
+                        </p>
+                    </footer>
+
+                    
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Nome</span>
+                            <span className="place">place</span>
+                        </div>
+                        <img src={more} alt="Mais"></img>
+                    </header>
+                    <img src="http://localhost:3333/files/Fernando-de-Noronha.jpg" alt="" />
+                
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+                    </footer>
+
+                    <strong>900 curtidas</strong>
+                    <p>
+                        Descrição do post!
+                        <span>#Estudo #React</span>
+                    </p>
+                </article>
+            </section>
+            );
+    }
+}
+
+export default Feed;
